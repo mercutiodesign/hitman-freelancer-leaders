28d083ab1a19b75c3e3e8fe81f22a1cf1250dac8
flamewing <flamewing.sonic@gmail.com>
2024-11-24 18:14:48 +0100

    Adding overlay version

---
+++ update-ui.js
@@ -0,0 +1,28 @@
+function setup() {
+    document.body.style.setProperty('--scrollbar-width', (window.innerWidth - document.body.clientWidth) + 'px')
+    update();
+}
+
+function update() {
+    var earrings = document.getElementById("earrings").checked;
+    var glasses = document.getElementById("glasses").checked;
+    var hat = document.getElementById("hat").checked;
+    var necklace = document.getElementById("necklace").checked;
+    var tattoo = document.getElementById("tattoo").checked;
+    var hair = document.querySelector('input[name="toggle_option"]:checked').id;
+    var image_ids = document.getElementsByClassName("leader");
+    for (var i = 0; i < image_ids.length; i++) {
+        var image = image_ids[i];
+        var earrings_match = image.getAttribute("data-earrings") == "true" || earrings == false;
+        var glasses_match = image.getAttribute("data-glasses") == "true" || glasses == false;
+        var hat_match = image.getAttribute("data-hat") == "true" || hat == false;
+        var necklace_match = image.getAttribute("data-necklace") == "true" || necklace == false;
+        var tattoo_match = image.getAttribute("data-tattoo") == "true" || tattoo == false;
+        var hair_match = image.getAttribute("data-hair") == hair || hair == "any";
+        if (earrings_match && glasses_match && hat_match && necklace_match && tattoo_match && hair_match) {
+            image.style.display = "block";
+        } else {
+            image.style.display = "none";
+        }
+    }
+}
